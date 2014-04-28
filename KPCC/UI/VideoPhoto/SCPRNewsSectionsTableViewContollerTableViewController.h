e66635a4d72344c5b93a87264b296f7285c1372d
John Meeker <jmeeker516@gmail.com>
2014-04-28 11:47:48 -0700

    Create Sections table view controller

---
+++ KPCC/UI/VideoPhoto/SCPRNewsSectionsTableViewContollerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCPRNewsSectionsTableViewContollerTableViewController.h
+//  KPCC
+//
+//  Created by John Meeker on 4/28/14.
+//  Copyright (c) 2014 scpr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCPRNewsSectionsTableViewContollerTableViewController : UITableViewController
+
+@end
